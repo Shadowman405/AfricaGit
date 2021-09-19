5bde24ffa116e0f2c398a1d2fccc52906392aa44
Maxim Mitin <maximus405@gmail.com>
2021-09-19 23:11:15 +0300

    Initial Commit

---
+++ AfricaGit/AfricaGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaGitApp.swift
+//  AfricaGit
+//
+//  Created by Maxim Mitin on 19.09.21.
+//
+
+import SwiftUI
+
+@main
+struct AfricaGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AfricaGit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AfricaGit
+//
+//  Created by Maxim Mitin on 19.09.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
